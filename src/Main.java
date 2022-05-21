d027e3664d40d5a97dc90ff47a84946ac0f6e838
Anna Melnikova <anna.melnikova91@bk.ru>
2022-05-21 18:16:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 500;
+        int amount = 20;
+
+        int amountMile = price / amount;
+
+        System.out.println("Количество бонусных милей: " + amountMile);
+    }
+}
